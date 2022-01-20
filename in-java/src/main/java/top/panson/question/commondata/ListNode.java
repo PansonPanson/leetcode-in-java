779e9f5a0229810f85ed2762433f42e8d0ba3d50
yuebai <yuebai@qunhemail.com>
2022-01-20 15:27:42 +0800

    反转链表

--- in-java/src/main/java/top/panson/question/commondata/ListNode.java
+++ in-java/src/main/java/top/panson/question/commondata/ListNode.java
@@ -1,23 +1,24 @@
 package top.panson.question.commondata;
 
 
+import top.panson.question.L002;
 
 /**
  * @create 2022-01-14 00:04
  * @Author: Panson
  */
 public class ListNode {
-    int val;
-    ListNode next;
+    public int val;
+    public ListNode next;
 
     ListNode() {
     }
 
-    ListNode(int val) {
+    public ListNode(int val) {
         this.val = val;
     }
 
-    public ListNode(int val, ListNode next) {
+    ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
     }
