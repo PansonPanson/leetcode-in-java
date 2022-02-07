d10ce83eeaaf742b1cfcfd533b1b3c745823fa88
yuebai <yuebai@qunhemail.com>
2022-02-07 22:15:14 +0800

    L102

---
+++ in-java/src/main/java/top/panson/injava/question/practice/package-info.java
@@ -0,0 +1,6 @@
+package top.panson.injava.question.practice;
+
+/**
+ * 分类合集：
+ * 1. BFS:  https://leetcode-cn.com/problems/binary-tree-level-order-traversal/solution/bfs-de-shi-yong-chang-jing-zong-jie-ceng-xu-bian-l/
+ */
