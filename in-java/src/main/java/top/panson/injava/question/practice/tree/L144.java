df8ced1e4b01dc33c2bf1605e4b51dcbe41a89eb
Panson <PansonPanson@163.com>
2022-03-07 00:48:04 +0800

    L144

--- in-java/src/main/java/top/panson/injava/question/practice/tree/L144.java
+++ in-java/src/main/java/top/panson/injava/question/practice/tree/L144.java
@@ -56,4 +56,29 @@ public class L144 {
             return res;
         }
     }
+
+    class Solution3 {
+        public List<Integer> preorderTraversal(TreeNode root) {
+            List<Integer> res = new ArrayList<>();
+            if(root == null) {
+                return res;
+            }
+            Deque<TreeNode> stack = new LinkedList<>();
+            stack.push(root);
+            while(!stack.isEmpty()) {
+                TreeNode node = stack.pop();
+                res.add(node.val);
+                if(node.right != null) {
+                    stack.push(node.right);
+                }
+                if(node.left != null) {
+                    stack.push(node.left);
+                }
+            }
+
+            return res;
+        }
+    }
+
+
 }
