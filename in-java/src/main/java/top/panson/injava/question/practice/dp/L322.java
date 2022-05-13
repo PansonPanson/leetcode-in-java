cf1aa26e02d2b5b2e6c2fb6af6a45c0c38d4b92e
Panson <PansonPanson@163.com>
2022-05-13 22:55:49 +0800

    dp

--- in-java/src/main/java/top/panson/injava/question/practice/dp/L322.java
+++ in-java/src/main/java/top/panson/injava/question/practice/dp/L322.java
@@ -24,4 +24,37 @@ public class L322 {
             return dp[amount] == amount + 1 ? -1 : dp[amount];
         }
     }
+
+    class Solution01 {
+        int[] memo;
+
+        public int coinChange(int[] coins, int amount) {
+            memo = new int[amount + 1];
+            Arrays.fill(memo, -100);
+            return dp(coins, amount);
+        }
+
+        public int dp(int[] coins, int amount) {
+            if(amount == 0) {
+                return 0;
+            }
+            if(amount < 0) {
+                return -1;
+            }
+            if(memo[amount] != -100) {
+                return memo[amount];
+            }
+            int res = Integer.MAX_VALUE;
+
+            for(int i = 0; i < coins.length; i++) {
+                int subProblem = dp(coins, amount - coins[i]);
+                if(subProblem == -1) {
+                    continue;
+                }
+                res = Math.min(res, 1 + dp(coins, amount - coins[i]));
+            }
+            memo[amount] =  (res == Integer.MAX_VALUE) ? -1 : res;
+            return memo[amount];
+        }
+    }
 }
