76a8d64e94b25243e2b40617b095480603ca5d5b
Panson <PansonPanson@163.com>
2022-05-30 20:06:06 +0800

    L021

--- in-java/src/main/java/top/panson/injava/question/practice/list/L021.java
+++ in-java/src/main/java/top/panson/injava/question/practice/list/L021.java
@@ -36,16 +36,10 @@ public class L021 {
             }
             while(list1 != null) {
                 dummy.next = list1;
-                list1 = list1.next;
-                dummy = dummy.next;
-
             }
 
             while(list2 != null) {
                 dummy.next = list2;
-                list2 = list2.next;
-                dummy = dummy.next;
-
             }
             return head.next;
         }
