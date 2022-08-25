24f6794ca19904da1dc6c757bddc52d0aa35b65f
PansonPanson <PansonPanson@163.com>
2022-08-25 17:13:38 +0800

    L658

--- in-java/src/main/java/top/panson/injava/question/practice/array/L658.java
+++ in-java/src/main/java/top/panson/injava/question/practice/array/L658.java
@@ -27,4 +27,33 @@ public class L658 {
             return res;
         }
     }
+
+    public class Solution1 {
+
+        public List<Integer> findClosestElements(int[] arr, int k, int x) {
+            int size = arr.length;
+            int left = 0;
+            int right = size - k;
+            while (left < right) {
+                // int mid = left + (right - left) / 2;
+                int mid = (left + right) / 2;
+                // 尝试从长度为 k + 1 的连续子区间删除一个元素
+                // 从而定位左区间端点的边界值
+                if (x - arr[mid] > arr[mid + k] - x) {
+                    // 下一轮搜索区间是 [mid + 1..right]
+                    left = mid + 1;
+                } else {
+                    // 下一轮搜索区间是 [left..mid]
+                    right = mid;
+                }
+            }
+
+            List<Integer> res = new ArrayList<>();
+            for (int i = left; i < left + k; i++) {
+                res.add(arr[i]);
+            }
+            return res;
+        }
+    }
+
 }
